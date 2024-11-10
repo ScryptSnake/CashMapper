1d17f024770094b4a6773894392b3ef089d1ada4
BM <bmiller.pct@gmail.com>
2024-11-10 18:03:51 -0500

    Wrote some more unit tests for category repository.

--- TestProject/Fixtures/DatabaseFixture.cs
+++ TestProject/Fixtures/DatabaseFixture.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using CashMapper.DataAccess.Entities;
 using CashMapper.DataAccess.Repositories;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Bson;
 using TestProject.Fixtures;
 
 
@@ -16,8 +17,11 @@ namespace TestProject.Fixtures;
 public class RepositoryFixture : IDisposable
 {
 
-    private const string databasePath =
+    private const string DATABASE_PATH =
         @"C:\Users\WIN11PC\source\repos\CashMapper\TestProject\Resources\test_database.db";
+
+    private IDatabaseFactory DatabaseFactory { get; }
+
     public IRepository<IncomeItem> IncomeItemRepo { get; }
     public IRepository<BudgetItem> BudgetItemRepo { get; }
     public IRepository<CashflowEntry> CashflowEntryRepo { get; }
@@ -27,25 +31,68 @@ public class RepositoryFixture : IDisposable
     public IRepository<AccountProfile> AccountProfileRepo { get; }
     public IRepository<Category> CategoryRepo { get; }
 
-
     public RepositoryFixture()
     {
+
         // Create DB settings.
-        var settings = new DatabaseSettings() { Path = databasePath };
+        var settings = new DatabaseSettings() { Path = DATABASE_PATH };
         var options = Options.Create(settings);
 
         // Get a factory.
-        var dbFactory = new DatabaseFactory(options);
+        DatabaseFactory = new DatabaseFactory(options);
 
         // Instantiate Repos.
-        IncomeItemRepo = new IncomeItemRepository(dbFactory);
-        BudgetItemRepo = new BudgetItemRepository(dbFactory);
-        CashflowEntryRepo = new CashflowEntryRepository(dbFactory);
-        TransactionRepo = new TransactionRepository(dbFactory);
-        IncomeProfileRepo = new IncomeProfileRepository(dbFactory);
-        ExpenseItemRepo = new ExpenseItemRepository(dbFactory);
-        AccountProfileRepo = new AccountProfileRepository(dbFactory);
-        CategoryRepo = new CategoryRepository(dbFactory);
+        IncomeItemRepo = new IncomeItemRepository(DatabaseFactory);
+        BudgetItemRepo = new BudgetItemRepository(DatabaseFactory);
+        CashflowEntryRepo = new CashflowEntryRepository(DatabaseFactory);
+        TransactionRepo = new TransactionRepository(DatabaseFactory);
+        IncomeProfileRepo = new IncomeProfileRepository(DatabaseFactory);
+        ExpenseItemRepo = new ExpenseItemRepository(DatabaseFactory);
+        AccountProfileRepo = new AccountProfileRepository(DatabaseFactory);
+        CategoryRepo = new CategoryRepository(DatabaseFactory);
+
+        // Prepare database for testing.
+        Task.Run(() => InitializeDataAsync()).GetAwaiter().GetResult();
+    }
+
+    private async Task InitializeDataAsync()
+    {
+        // Prepares the database for tests.
+        BackupDatabase();
+        await WipeDatabaseAsync();
+    }
+
+
+    private void BackupDatabase()
+    {
+        string localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        var destination = $"{localAppData}\\database_backup";
+
+        File.Copy(DATABASE_PATH, destination, true);
+        
+    }
+
+    private async Task WipeDatabaseAsync()
+    {
+        // Grab database to execute sql. 
+        var db = await DatabaseFactory.GetDatabase();
+
+        // Get list of tables. 
+        var sql = "SELECT name FROM sqlite_master WHERE type='table';";
+        var tables = await db.GetMultipleAsync<string>(sql);
+
+        // Disable constraints.
+        await db.ExecuteAsync("PRAGMA foreign_keys = OFF;");
+
+
+        // Wipe each table. 
+        foreach (var table in tables)
+        {
+            var query = $"DELETE FROM {table};";
+            await db.ExecuteAsync(query);
+        }
+        // Enable constraints.
+        await db.ExecuteAsync("PRAGMA foreign_keys = ON;");
     }
 
     public void Dispose()
--- TestProject/Tests/CategoryRepositoryTests.cs
+++ TestProject/Tests/CategoryRepositoryTests.cs
@@ -3,45 +3,88 @@ using CashMapper.DataAccess.Repositories;
 using CashMapper.Enums;
 using TestProject.Fixtures;
 
-namespace TestProject.Tests
+namespace TestProject.Tests;
+
+/// <summary>
+/// A test class for the income item repository.
+/// </summary>
+
+public class CategoryRepositoryTests : IClassFixture<RepositoryFixture>
 {
-    /// <summary>
-    /// A test class for the income item repository.
-    /// </summary>
+    private RepositoryFixture Fixture { get; }
+    private IRepository<Category> Repository { get; }
 
-    public class CategoryRepositoryTests : IClassFixture<RepositoryFixture>
+    public CategoryRepositoryTests(RepositoryFixture fixture)
     {
-        private RepositoryFixture Fixture { get; }
-        private IRepository<Category> Repository { get; }
-
-        public CategoryRepositoryTests(RepositoryFixture fixture)
-        {
-            // Set fixture.
-            Fixture = fixture;
-            Repository = fixture.CategoryRepo;
-        }
-
-
-        [Fact]
-        public async void CategoryRepository_AddAsync_ShouldReturnMatch()
-        {
-            // Arrange.
-            var inputEntity = new Category()
-            {
-                Name = "TestCategory3",
-                CategoryType = CategoryTypes.Budget,
-                Flag = "FLAG",
-            };
-
-            // Act.
-            var outputEntity = await Repository.AddAsync(inputEntity);
-
-            // Assert.
-            // Note: Some properties cannot be tested. Any database generated fields.
-            // These include:  Id, DateCreated, DateModified.
-            Assert.Equal(inputEntity.Name, outputEntity.Name);
-            Assert.Equal(inputEntity.CategoryType, outputEntity.CategoryType);
-            Assert.Equal(inputEntity.Flag,outputEntity.Flag);
-        }
+        // Set fixture.
+        Fixture = fixture;
+        Repository = fixture.CategoryRepo;
     }
+
+    [Fact]
+    public async void CategoryRepository_AddAsync_ShouldMatch()
+    {
+        // Arrange.
+        var inputEntity = new Category()
+        {
+            Name = "TestCategory3",
+            CategoryType = CategoryTypes.Budget,
+            Flag = "FLAG",
+        };
+
+        // Act.
+        var outputEntity = await Repository.AddAsync(inputEntity);
+
+        // Assert.
+        // Note: Some properties cannot be tested. Any database generated fields.
+        // We do not know their values at design time. 
+        // These include:  Id, DateCreated, DateModified.
+        Assert.Equal(inputEntity.Name, outputEntity.Name);
+        Assert.Equal(inputEntity.CategoryType, outputEntity.CategoryType);
+        Assert.Equal(inputEntity.Flag,outputEntity.Flag);
+    }
+
+    [Fact]
+    public async void CategoryRepository_FindAsync_ShouldMatch()
+    {
+        // Arrange,
+        var newEntity = new Category()
+        {
+            Name = "TestCategory4",
+            CategoryType = CategoryTypes.Budget,
+            Flag = "FLAG",
+        };
+
+        // Act.
+        var addEntity = await Repository.AddAsync(newEntity);
+        var id = addEntity.Id;
+        var outputEntity = await Repository.FindAsync(id);
+
+        // Assert.
+        Assert.Equal(addEntity.Id, outputEntity.Id);
+        Assert.Equal(outputEntity.CategoryType, newEntity.CategoryType);
+        Assert.Equal(outputEntity.Flag, newEntity.Flag);
+        Assert.Equal(outputEntity.Name, newEntity.Name);
+
+    }
+
+    [Fact]
+    public async void CategoryRepository_ExistsAsync_ShouldBeTrue()
+    {
+        // Arrange.
+        var newEntity = new Category()
+        {
+            Name = "TestCategory5",
+            CategoryType = CategoryTypes.Budget,
+            Flag = "FLAG",
+        };
+
+        // Act.
+        var addEntity = await Repository.AddAsync(newEntity);
+        var exists = await Repository.ExistsAsync(addEntity);
+
+        // Assert.
+        Assert.True(exists);
+    }
+
 }
